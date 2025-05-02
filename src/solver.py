37b84547590b4180ab3719c6a2fe2e735929b779
Carlos Martinez <jcmartinezmori@ipsec-10-2-225-48.vpn.gatech.edu>
2025-05-02 12:29:32 -0400

    .

--- src/solver.py
+++ src/solver.py
@@ -14,7 +14,7 @@ def main(instance, modelname, **kwargs):
 
     m = gp.Model()
     m.Params.OutputFlag = kwargs.get('OutputFlag', 1)
-    m.Params.FeasibilityTol = kwargs.get('FeasibilityTol', 1E-9)
+    m.Params.FeasibilityTol = kwargs.get('FeasibilityTol', 1E-6)
     m.Params.NumericFocus = kwargs.get('NumericFocus', 3)
     m.Params.OptimalityTol = kwargs.get('OptimalityTol', 1E-9)
     m.ModelSense = -1
@@ -111,6 +111,7 @@ def get_blocking(instance, u_N, **kwargs):
 
     m_S = gp.Model()
     m_S.Params.OutputFlag = kwargs.get('OutputFlag', 1)
+    m_S.Params.FeasibilityTol = kwargs.get('FeasibilityTol', 1E-9)
     m_S.Params.MIPFocus = kwargs.get('MIPFocus', 1)
     m_S.Params.NumericFocus = kwargs.get('NumericFocus', 3)
     m_S.Params.TimeLimit = kwargs.get('TimeLimit', 60)
@@ -154,7 +155,9 @@ def get_intersections(instance, m, u_N, S, **kwargs):
 
     m_S = gp.Model()
     m_S.Params.OutputFlag = kwargs.get('OutputFlag', 1)
+    m_S.Params.FeasibilityTol = kwargs.get('FeasibilityTol', 1E-9)
     m_S.Params.NumericFocus = kwargs.get('NumericFocus', 3)
+    m_S.Params.OptimalityTol = kwargs.get('OptimalityTol', 1E-9)
     m_S.ModelSense = -1
 
     m_S._lam = m_S.addVar(vtype=gp.GRB.CONTINUOUS, lb=0, ub=gp.GRB.INFINITY, name='lam')
