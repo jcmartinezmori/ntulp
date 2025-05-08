c3f8a659f4f29b32656d3a1e61db04c18cfb3570
Carlos Martinez <jcmartinezmori@ipsec-10-2-225-153.vpn.gatech.edu>
2025-05-07 21:48:24 -0400

    .

--- src/solver.py
+++ src/solver.py
@@ -116,14 +116,21 @@ def get_blocking(instance, u_N, **kwargs):
     Starts = kwargs.get('blocking_Starts', {tuple(sorted(N))})
 
     for j in J:
+
+        eps_j = 0
+        S_j = None
+
         A_j = A[0][j]
         N_j_V_j = sorted([(i, V[i][j]) for i in N], key=lambda x: x[1], reverse=True)
         for ct in range(1, len(N) + 1):
             if N_j_V_j[ct - 1][1] <= 0:
                 break
-            if min(V_ij * ct / A_j - u_N[i] for i, V_ij in N_j_V_j[:ct]) > 0:
-                S = tuple(sorted(i for i, _ in N_j_V_j[:ct]))
-                Starts.add(S)
+            eps = min(V_ij * ct / A_j - u_N[i] for i, V_ij in N_j_V_j[:ct])
+            if eps > eps_j:
+                eps_j = eps
+                S_j = tuple(sorted(i for i, _ in N_j_V_j[:ct]))
+        if S_j is not None:
+            Starts.add(S_j)
 
     m_S = gp.Model()
     m_S.Params.OutputFlag = kwargs.get('OutputFlag', 1)
