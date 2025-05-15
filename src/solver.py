45b7423f8275a61aa15a35d41ef83db95b000baf
Carlos Martinez <jcmartinezmori@ipsec-10-2-96-67.vpn.gatech.edu>
2025-05-14 23:32:32 -0400

    .

--- src/solver.py
+++ src/solver.py
@@ -88,6 +88,7 @@ def main(instance, modelname, **kwargs):
 
         print('... adding cut for current S.')
         intersections = get_intersections(instance, m, u_N, S, LamTh=0)
+        print(intersections)
         if intersections is not None:
             cutCount += 1
             s = m.addVar(vtype=gp.GRB.CONTINUOUS, lb=0, ub=gp.GRB.INFINITY)
