afb9db6258a3d4e25cc4862da7898429c2960d97
Carlos Martinez <jcmartinezmori@ipsec-10-2-226-174.vpn.gatech.edu>
2025-05-20 09:08:58 -0400

    .

--- src/solver.py
+++ src/solver.py
@@ -102,35 +102,38 @@ def main(instance, modelname, **kwargs):
                 )
                 print('......... stored seemingly singular basis.')
 
+        print('... adding cuts for previous S.')
+        cutPrev = False
+        for prev_S in Starts:
+            if prev_S == S:
+                continue
+            intersections = get_intersections(
+                instance, m, constr_names_to_indices, basis_mat, basis_varnames, u_N, prev_S,
+                epsTh=1E-3, lamRatTh=1E-7
+            )
+            if intersections is not None:
+                cutPrev = True
+                min_lam = min(lam for _, lam in intersections)
+                max_lam = max(lam for _, lam in intersections)
+                s = m.addVar(vtype=gp.GRB.CONTINUOUS, lb=0, ub=gp.GRB.INFINITY, name='s[{0}]'.format(slackCount))
+                slackCount += 1
+                m.addConstr(gp.quicksum(m.getVarByName(varname) / lam for varname, lam in intersections) - s == 1)
+                cutCount += 1
+                print('...... added cut for prev. S with coeff. ratio {0}.'.format(min_lam/max_lam))
         print('... adding cut for current S.')
         intersections = get_intersections(
             instance, m, constr_names_to_indices, basis_mat, basis_varnames, u_N, S,
             epsTh=0, lamRatTh=0
         )
         if intersections is not None:
-            cutCount += 1
-            s = m.addVar(vtype=gp.GRB.CONTINUOUS, lb=0, ub=gp.GRB.INFINITY, name='s[{0}]'.format(slackCount))
-            slackCount += 1
-            m.addConstr(gp.quicksum(m.getVarByName(varname) / lam for varname, lam in intersections) - s == 1)
             min_lam = min(lam for _, lam in intersections)
             max_lam = max(lam for _, lam in intersections)
-            print('...... added cut for curr. S with coeff. ratio {0}.'.format(min_lam / max_lam))
-        print('... adding cuts for previous S.')
-        for prev_S in Starts:
-            if prev_S == S:
-                continue
-            intersections = get_intersections(
-                instance, m, constr_names_to_indices, basis_mat, basis_varnames, u_N, prev_S,
-                epsTh=1E-3, lamRatTh=1E-6
-            )
-            if intersections is not None:
-                cutCount += 1
+            if min_lam/max_lam >= 1E-7 or not cutPrev:
                 s = m.addVar(vtype=gp.GRB.CONTINUOUS, lb=0, ub=gp.GRB.INFINITY, name='s[{0}]'.format(slackCount))
                 slackCount += 1
                 m.addConstr(gp.quicksum(m.getVarByName(varname) / lam for varname, lam in intersections) - s == 1)
-                min_lam = min(lam for _, lam in intersections)
-                max_lam = max(lam for _, lam in intersections)
-                print('...... added cut for prev. S with coeff. ratio {0}.'.format(min_lam/max_lam))
+                cutCount += 1
+                print('...... added cut for curr. S with coeff. ratio {0}.'.format(min_lam / max_lam))
         Starts.add(S)
         print('... solving model.')
 
