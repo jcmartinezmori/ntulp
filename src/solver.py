9399941b9d8bacdeca7d5194279745562b83261b
Carlos Martinez <jcmartinezmori@ipsec-10-2-96-86.vpn.gatech.edu>
2025-05-29 15:56:58 -0400

    .

--- src/solver.py
+++ src/solver.py
@@ -187,7 +187,7 @@ def get_blocking(instance, u_N, **kwargs):
     m_S = gp.Model()
     m_S.Params.OutputFlag = kwargs.get('OutputFlag', 1)
     m_S.Params.FeasibilityTol = kwargs.get('FeasibilityTol', 1E-6)
-    m_S.Params.MIPFocus = kwargs.get('MIPFocus', 1)
+    m_S.Params.MIPFocus = kwargs.get('MIPFocus', 0)
     m_S.Params.NumericFocus = kwargs.get('NumericFocus', 3)
     m_S.NumStart = len(Starts)
     m_S.ModelSense = -1
