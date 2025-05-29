c68fc4d727f87e490d0be564d85e33f10551cad5
Carlos Martinez <jcmartinezmori@ipsec-10-2-96-86.vpn.gatech.edu>
2025-05-29 15:12:26 -0400

    .

--- src/config.py
+++ src/config.py
@@ -4,8 +4,24 @@ BASIC = 0
 CBASIC = 0
 
 # --- instance parameters --- #
+LINES_DIST_TRGT = 400  # meters
+LINES_DIST_CTFF = 1600  # meters
+TRIP_MILES_LB = 1/2  # miles
+TRIP_SECS_LB = 60  # seconds
 PLACE = 'Cook County'
 FILENAME = PLACE.replace(' ', '-')
+ADMIN_LEVEL = 6
+CUSTOM_FILTER = (
+    '['
+    '"highway"~'
+    '"motorway|trunk|primary|secondary|tertiary|motorway_link|trunk_link|primary_link|secondary_link|tertiary_link|residential"'
+    ']'
+)
+STOPS_TAGS = {
+    'highway': 'bus_stop'
+}
+AIRPORT_NODES = {310333647, 365195019}
+
 
 # --- plotting parameters --- #
 CENTER = (41.8781, -87.6298)
